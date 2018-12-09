46cf9faeb679f2bca085a221c2496fa751edfc8e
Brienyll <brienyll29@gmail.com>
2018-12-09 12:05:26 -0800

    added clarifai api

--- src/App.js
+++ src/App.js
@@ -5,19 +5,46 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '1c198088de08489eb8b33107e8d9d9c8'
+ });
 
 const particlesOptions = {
   particles: {
-    line_linked: {
-      shadow: {
+    number: {
+      value: 100,
+      density: {
         enable: true,
-        color: "#3CA9D1",
-        blur: 5
+        value_area:800
       }
     }
   }
 }
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+  onInputChange = (e) => {
+    console.log(e.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+      app.models.predict("c0c0ac362b03416da06ab3fa36fb58e3", "https://samples.clarifai.com/demographics.jpg").then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -27,7 +54,9 @@ class App extends Component {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm 
+        onInputChange={this.onInputChange}
+        onButtonSubmit={this.onButtonSubmit}/>
       {/*<FaceRecognition />*/}
       </div>
     );
