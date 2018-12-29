97f4798e9366bafa807a1a6af6be475ed84472e3
Brienyll <brienyll29@gmail.com>
2018-12-29 11:14:17 -0800

    added profile update

--- src/App.js
+++ src/App.js
@@ -80,7 +80,22 @@ class App extends Component {
       app.models.predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count}))
+        })
+      }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
   }
 
@@ -104,7 +119,10 @@ class App extends Component {
       { route === 'home' 
       ? <div>
           <Logo />
-          <Rank />
+          <Rank 
+            name={this.state.user.name}
+            entries={this.state.user.entries}
+          />
           <ImageLinkForm 
             onInputChange={this.onInputChange}
             onButtonSubmit={this.onButtonSubmit}/>
@@ -112,7 +130,7 @@ class App extends Component {
         </div>
         : (
           route === 'signin'
-          ? <Signin onRouteChange={this.onRouteChange}/>
+          ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
           : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
         )
       }
